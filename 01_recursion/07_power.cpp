cc554ea429c4283397353ba9a1fa025b246674e4
manjunath <bmanjunath728@gmail.com>
2022-10-24 06:54:42 +0530

    update

--- 01_recursion/07_power.cpp
+++ 01_recursion/07_power.cpp
@@ -7,7 +7,18 @@ int pow(int m,int n){
         return pow(m,n-1)*m;
     }
 }
+int pow1(int m,int n){
+    if(n==0)
+    {
+        return 1;
+    } else if(n%2==0){
+        return pow(m*m,n/2);
+    }else{
+        return m*pow(m*m,n/2);
+    }
+}
 int main(){ 
-    cout<<pow(3,2);// 6 power of 3
+    cout<<pow(3,2)<<endl;// 6 power of 3
+    cout<<pow1(3,2)<<endl;// 6 power of 3
     return 0;
 }
